179980a8fe3f8aa1a543ae48d9001567649d5e29
BlackWarrior06v <curipch2@gmail.com>
2024-12-20 17:21:13 -0600

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Curi on 20/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
